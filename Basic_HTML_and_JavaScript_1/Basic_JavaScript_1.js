54254962688857a7b1a4f754924e85536a0e0d2e
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-05-02 21:17:14 -0400

    submitting changes for this assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("hello,world!")
+function My_First_Function() }
+	var String = "Kiss me, I'm Irish!";
+	var result = String.frontcolor("green");
+	document.getElementById("Irish").innerHTML=
+	result;}
